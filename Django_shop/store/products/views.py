39860c8c3a1db1f7167090785dca3b8f85081bba
Hayse3101 <lolartem999@gmail.com>
2024-06-30 22:37:23 +0300

    commit and push 30.06.2024 - 22:36

---
+++ Django_shop/store/products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'products/index.html')
+
+
+def product(request):
+    return render(request, 'products/products.html')
