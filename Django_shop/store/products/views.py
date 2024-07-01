bb786da7951bfa8a0ea55d7e249f44d2d0bcc703
Hayse3101 <lolartem999@gmail.com>
2024-07-01 16:52:10 +0300

    commit and push 01.07.2024 - 16:52

--- Django_shop/store/products/views.py
+++ Django_shop/store/products/views.py
@@ -9,3 +9,22 @@ def index(request):
 
 def product(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'Store',
+        'header': 'Добро пожаловать!',
+        'user_name': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+        ],
+    }
+
+    return render(request, 'products/test_context.html', context=context)
