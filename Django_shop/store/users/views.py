095d21291ed65b0e19f0a15a44e76fdc6838ed33
Hayse3101 <lolartem999@gmail.com>
2024-07-10 13:16:52 +0300

    commit and push 10.07.2024 - 13:16

--- Django_shop/store/users/views.py
+++ Django_shop/store/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm
+from django.contrib import auth
 
 # Create your views here.
+def login(requset):
+    if requset.method == 'POST':
+        form = UserLoginForm(data=requset.POST)
+        if form.is_valid():
+            username = requset.POST['username']
+            password = requset.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(requset, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(requset, 'users/login.html', context)
+
+
+def register(request):
+    return render(request, 'users/register.html')
+
